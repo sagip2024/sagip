bdebf3d123a0bd712a780369ad16af6c8d3c64d4
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-11-10 13:09:13 +0800

    Update

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,7 +5,6 @@ function Icons({ bgColor, icon, name, borderColor,hoverBg, pageId, image }) {
     <>
       <a href={`#${pageId}`}>
         <div className={`border ${borderColor} ${bgColor} w-40 h-40 sm:w-32 sm:h-32 grid  place-items-center rounded-[30%] cursor-pointer relative ${hoverBg} overflow-hidden`}>
-          <img src={Lines} alt="Sagip Icon Background Lines" />
           <img src={image} alt="Sagip Icons" className='w-32 absolute sm:w-24'/>
           <p className="absolute bottom-3 sm:bottom-1 font-bold p-2 text-center sm:text-sm">{name}</p>
         </div>
